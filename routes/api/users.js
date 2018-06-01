0f04d80fbdd88bda998de45323fd27848d1e852e
tarekcham <tarek.alkhatib1@gmail.com>
2018-06-01 23:55:40 +0200

    registration and login backend are done

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,73 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+
+// Load User model
+const User = require("../../models/User");
 
 // @route   Get api/users/test
 // #desc    Tests users route
 // @access  Public
 
-router.get('/test', (req,res) => res.json({msg: "users Works"}));
+router.get("/test", (req, res) => res.json({ msg: "users Works" }));
 
+// @route   Get api/users/register
+// #desc    Register user
+// @access  Public
+
+router.post("/register", (req, res) => {
+	User.findOne({ email: req.body.email }).then(user => {
+		if (user) {
+			return res.status(400).json({ email: "email already exists" });
+		} else {
+			const avatar = gravatar.url(req.body.email, {
+				s: 200, // Size
+				r: "pg", // Rating
+				d: "mm" // Default
+			});
+
+			const newUser = new User({
+				name: req.body.name,
+				email: req.body.email,
+				avatar,
+				password: req.body.password
+			});
+			bcrypt.genSalt(10, (err, salt) => {
+				bcrypt.hash(newUser.password, salt, (err, hash) => {
+					if (err) throw err;
+					newUser.password = hash;
+					newUser
+						.save()
+						.then(user => res.json(user))
+						.catch(err => console.log(err));
+				});
+			});
+		}
+	});
+});
+
+// @route   Get api/users/login
+// #desc    Login user / Returning  JWT Tocken
+// @access  Public
+router.post("/login", (req, res) => {
+	const { email } = req.body;
+	const { password } = req.body;
+
+	// Find user by email
+	User.findOne({ email }).then(user => {
+		if (!user) {
+			return res.status(404).json({ email: "User not found" });
+		}
+
+		// Check Password
+		bcrypt.compare(password, user.password).then(isMatch => {
+			if (isMatch) {
+				res.json({ msg: "Succuss" });
+			} else {
+				return res.status(400).json({ password: "Password incorrect" });
+			}
+		});
+	});
+});
 module.exports = router;
-
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/users');
@@ -7,6 +8,11 @@ const posts = require('./routes/api/posts');
 
 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({extended: false})); //basically tells the system whether you want to use a simple algorithm for shallow parsing (i.e. false) or complex algorithm for deep parsing that can deal with nested objects (i.e. true).
+app.use(bodyParser.json()); // basically tells the system that you want json to be used.
+
 
 //DB Config
 const db = require('./config/keys').mongoURI;
