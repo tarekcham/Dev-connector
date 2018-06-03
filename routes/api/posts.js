16726170a337bf8d0d23537d71c14832fc3df841
tarekcham <tarek.alkhatib1@gmail.com>
2018-06-03 21:59:32 +0200

    profile routers done

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-// @route   Get api/posts/test
-// #desc    Tests post route
+// @route   GET api/posts/test
+// @desc    Tests post route
 // @access  Public
-
-router.get('/test', (req,res) => res.json({msg: "posts Works"}));
+router.get('/test', (req, res) => res.json({ msg: 'Posts Works' }));
 
 module.exports = router;
-
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 
 const users = require('./routes/api/users');
-const profile = require('./routes/api/users');
+const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
 
